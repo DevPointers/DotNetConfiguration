c7be0be4ad06601eefa2a3f33a6ac63a6a02278e
Tarun Ahuja <ahujtarun@gmail.com>
2022-09-18 18:40:43 -0400

    Initial Commit

---
+++ ConfigurationBuilderExtension/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using ConfigurationBuilderExtension;
+
+IConfiguration config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddEnvironmentVariables()
+    .Build();
+
+Settings settings = config.GetSection("Settings").Get<Settings>();
+
+Console.WriteLine("Name: " + config.GetSection("Settings").GetValue<string>("Name"));
+Console.WriteLine("Topic: " + config.GetSection("Settings").GetValue<string>("Topic"));
+Console.WriteLine("Session: " + config.GetValue<string>("Settings:Session"));
+
+
+Console.WriteLine("Name: " + settings.Name);
+Console.WriteLine("Topic: " +  settings.Topic);
+Console.WriteLine("Session: " + settings.Session);
+
+
