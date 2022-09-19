adf8987cf717c4cc20eb9753647cfe29b1e9e4eb
Tarun Ahuja <ahujtarun@gmail.com>
2022-09-19 00:20:56 -0400

    Initial Commit

---
+++ HostingExtension/Program.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using IHost host = Host.CreateDefaultBuilder(args).Build();
+
+IConfiguration configuration = host.Services.GetRequiredService<IConfiguration>();
+
+Console.WriteLine("Name: " +  configuration.GetValue<string>("Settings:Name"));
+
